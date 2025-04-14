3c6588a847721d3d90eee2c7586130b7cedd4e73
Eventora <khalilabdelmoumen7@gmail.com>
2025-04-14 02:03:26 +0100

    Eventora Done pour La validation

---
+++ src/EventSubscriber/LocaleSubscriber.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\EventSubscriber;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class LocaleSubscriber implements EventSubscriberInterface
+{
+    private $defaultLocale;
+
+    public function __construct(string $defaultLocale = 'fr')
+    {
+        $this->defaultLocale = $defaultLocale;
+    }
+
+    public function onKernelRequest(RequestEvent $event): void
+    {
+        $request = $event->getRequest();
+        if ($locale = $request->getSession()->get('_locale')) {
+            $request->setLocale($locale);
+        } else {
+            $request->setLocale($this->defaultLocale);
+        }
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::REQUEST => [['onKernelRequest', 16]], // Ajusté la priorité
+        ];
+    }
+}
